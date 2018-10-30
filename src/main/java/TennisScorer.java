e452f032c85e96fa82406648fed71e2287c7421d
Bartomeu <bartomeugm@gmail.com>
2018-10-30 17:28:47 +0100

    refactor: collapse into very similar score arrays¨

--- src/main/java/TennisScorer.java
+++ src/main/java/TennisScorer.java
@@ -1,18 +1,19 @@
 public class TennisScorer {
     private int ballsWonByPlayer1;
     private int ballsWonByPlayer2;
-    private String[] player1Scores = new String[]{"love-love", "fifteen-love", "thirty-love","","Player 1 wins"};
-    private String[] player2Scores = new String[]{"love-love", "fifteen-love", "thirty-love","","Player 2 wins"};
+    private String[] player1Scores = new String[]{"love", "fifteen", "thirty","","Player 1 wins"};
+    private String[] player2Scores = new String[]{"love", "fifteen", "thirty","","Player 2 wins"};
 
     public String getMatchScore() {
         if(ballsWonByPlayer2 == ballsWonByPlayer1 && ballsWonByPlayer2 == 3){
             return "Deuce";
         }
-        if(ballsWonByPlayer1 != 0) {
-            return player1Scores[ballsWonByPlayer1];
-        } else {
+        if(ballsWonByPlayer2 == 4){
             return player2Scores[ballsWonByPlayer2];
+        } else if (ballsWonByPlayer1 == 4 ){
+            return player1Scores[ballsWonByPlayer1];
         }
+        return String.format("%s-%s", player1Scores[ballsWonByPlayer1], player2Scores[ballsWonByPlayer2]);
     }
 
     public void addPointToPlayer1() {
