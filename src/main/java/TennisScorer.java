808b6f6bce0169a1c8212dc1ce57793b845adf77
Bartomeu <bartomeugm@gmail.com>
2018-10-30 17:30:37 +0100

    refactor: remove player winning from the scores array

--- src/main/java/TennisScorer.java
+++ src/main/java/TennisScorer.java
@@ -1,19 +1,18 @@
 public class TennisScorer {
     private int ballsWonByPlayer1;
     private int ballsWonByPlayer2;
-    private String[] player1Scores = new String[]{"love", "fifteen", "thirty","","Player 1 wins"};
-    private String[] player2Scores = new String[]{"love", "fifteen", "thirty","","Player 2 wins"};
+    private String[] scores = new String[]{"love", "fifteen", "thirty",""};
 
     public String getMatchScore() {
         if(ballsWonByPlayer2 == ballsWonByPlayer1 && ballsWonByPlayer2 == 3){
             return "Deuce";
         }
         if(ballsWonByPlayer2 == 4){
-            return player2Scores[ballsWonByPlayer2];
-        } else if (ballsWonByPlayer1 == 4 ){
-            return player1Scores[ballsWonByPlayer1];
+            return "Player 2 wins";
+        } else if (ballsWonByPlayer1 == 4) {
+            return "Player 1 wins";
         }
-        return String.format("%s-%s", player1Scores[ballsWonByPlayer1], player2Scores[ballsWonByPlayer2]);
+        return String.format("%s-%s", scores[ballsWonByPlayer1], scores[ballsWonByPlayer2]);
     }
 
     public void addPointToPlayer1() {
