222a0365a1bfd34d786420bce8bf6aba1ecb67f5
Bartomeu <bartomeugm@gmail.com>
2018-10-30 17:48:55 +0100

    green: all cases covered. feature is functionally complete

--- src/main/java/TennisScorer.java
+++ src/main/java/TennisScorer.java
@@ -4,12 +4,18 @@ public class TennisScorer {
     private String[] scores = new String[]{"love", "fifteen", "thirty", "forty"};
 
     public String getMatchScore() {
-        if (ballsWonByPlayer2 == ballsWonByPlayer1 && ballsWonByPlayer2 == 3) {
+        if (ballsWonByPlayer2 == ballsWonByPlayer1 && ballsWonByPlayer2 >= 3) {
             return "Deuce";
         }
-        if (ballsWonByPlayer2 == 4) {
+        if (ballsWonByPlayer1 >= 4 && ballsWonByPlayer1 - 1 == ballsWonByPlayer2) {
+            return "advantage for player 1";
+        }
+        if (ballsWonByPlayer2 >= 4 && ballsWonByPlayer2 - 1 == ballsWonByPlayer1) {
+            return "advantage for player 2";
+        }
+        if (ballsWonByPlayer2 >= 4 && ballsWonByPlayer2 - ballsWonByPlayer1 >= 2) {
             return "Player 2 wins";
-        } else if (ballsWonByPlayer1 == 4) {
+        } else if (ballsWonByPlayer1 >= 4 && ballsWonByPlayer1 - ballsWonByPlayer2 >= 2) {
             return "Player 1 wins";
         }
         return String.format("%s-%s", scores[ballsWonByPlayer1], scores[ballsWonByPlayer2]);
--- src/test/java/TennisShould.java
+++ src/test/java/TennisShould.java
@@ -51,6 +51,65 @@ public class TennisShould {
 
         assertThat(tennisScorer.getMatchScore(), is("Player 1 wins"));
     }
+
+    @Test
+    public void show_player_1_has_advantage() {
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2(); //Deuce
+
+        tennisScorer.addPointToPlayer1(); //advantage for player 1
+
+        assertThat(tennisScorer.getMatchScore(), is("advantage for player 1"));
+    }
+    @Test
+    public void show_player_2_has_advantage() {
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2(); //Deuce
+
+        tennisScorer.addPointToPlayer2(); //advantage for player 2
+
+        assertThat(tennisScorer.getMatchScore(), is("advantage for player 2"));
+    }
+
+    @Test
+    public void show_player_1_has_advantage_when_the_players_get_back_to_the_deuce() {
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2(); // Deuce
+        tennisScorer.addPointToPlayer1(); // Adv player 1
+        tennisScorer.addPointToPlayer2(); // Deuce
+
+        tennisScorer.addPointToPlayer1(); // advantage for player 1
+
+        assertThat(tennisScorer.getMatchScore(), is("advantage for player 1"));
+    }
+
+    @Test
+    public void show_player_2_has_advantage_when_the_players_get_back_to_the_deuce() {
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2(); // Deuce
+        tennisScorer.addPointToPlayer1(); // Adv player 1
+        tennisScorer.addPointToPlayer2(); // Deuce
+
+        tennisScorer.addPointToPlayer2(); // advantage for player 2
+
+        assertThat(tennisScorer.getMatchScore(), is("advantage for player 2"));
+    }
     @Test
     public void show_player_2_wins_when_player_2_wins() {
         tennisScorer.addPointToPlayer2();
@@ -71,5 +130,53 @@ public class TennisShould {
         tennisScorer.addPointToPlayer1(); // 40
 
         assertThat(tennisScorer.getMatchScore(), is("Deuce"));
+    } @Test
+    public void deuce_when_players_have_reverted_the_advantage() {
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1(); // Deuce
+
+        tennisScorer.addPointToPlayer2(); // Advantage
+        tennisScorer.addPointToPlayer1(); // Reverted the advantage. Deuce again
+
+        assertThat(tennisScorer.getMatchScore(), is("Deuce"));
+    }
+
+
+
+    @Test
+    public void player_2_wins_according_to_the_rule_description() {
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2(); // Deuce
+        tennisScorer.addPointToPlayer1(); // Adv player 1
+        tennisScorer.addPointToPlayer2(); // Deuce
+
+        tennisScorer.addPointToPlayer2(); // advantage
+        tennisScorer.addPointToPlayer2(); // player wins
+
+        assertThat(tennisScorer.getMatchScore(), is("Player 2 wins"));
+    }
+    @Test
+    public void player_1_wins_according_to_the_rule_description() {
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2();
+        tennisScorer.addPointToPlayer1();
+        tennisScorer.addPointToPlayer2(); // Deuce
+        tennisScorer.addPointToPlayer1(); // Adv player 1
+        tennisScorer.addPointToPlayer2(); // Deuce
+
+        tennisScorer.addPointToPlayer1(); // advantage
+        tennisScorer.addPointToPlayer1(); // player wins
+
+        assertThat(tennisScorer.getMatchScore(), is("Player 1 wins"));
     }
 }
